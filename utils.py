2d4c4cd18c91cb2554cf7fdff06212c687f22509
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2022-09-28 14:12:36 -0300

    Change name function of imager filenames

--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import datetime 
 
 
-def filename_to_datetime(filename):
     """Convert filename attributes into datetime format"""
     if ".tif" in filename:
         infos = filename.replace(".tif", "").split("_")
@@ -11,7 +10,16 @@ def filename_to_datetime(filename):
     date = infos[2]
     time = infos[-1]
     return datetime.datetime.strptime(date + " " + time, '%Y%m%d %H%M%S')
+def imager_fname(filename):
+    """Convert filename attributes into datetime format"""
+    if ".tif" in filename:
+        infos = filename.replace(".tif", "").split("_")
+    else:
+        infos = filename.replace(".png", "").split("_")
 
+    date = infos[2]
+    time = infos[-1]
+    return datetime.datetime.strptime(date + " " + time, '%Y%m%d %H%M%S')
 
 def doy_str_format(date: int) -> str:
     """Convert integer to string. Ex: 1 to 001"""
