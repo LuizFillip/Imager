6ee15478d4d91c455dfc2067630aca04a9772f1a
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2022-10-01 23:27:11 -0300

    Removing functions for crop image

--- image_labeling.py
+++ image_labeling.py
@@ -1,9 +1,12 @@
 import cv2
 import os
 import matplotlib.pyplot as plt
-from image_settings import image_processing, crop_image_like_circle
+from image_settings import load_and_processing
 from image_utils import imager_fname
-
+import numpy as np
+import datetime
+import os
+from PIL import Image, ImageDraw, ImageFont
 
 def draw_labels(ax, 
                 filename, 
@@ -59,11 +62,10 @@ def image_visualization(infile, filename,
                         path_to_save = "",
                         save = False):
     
+    img = load_and_processing(infile, filename)
     
-    img = crop_image_like_circle(image_processing(infile, 
-                                                  filename))
+
    
-    
     if save:
         plt.ioff()
     
@@ -71,30 +73,21 @@ def image_visualization(infile, filename,
     
     ax.imshow(img)
     
-    draw_labels(ax, 
-                filename, 
-                fontsize = 20, 
-                color = "black", 
-                site = "CA", 
-                emisson = "O6")
+    ax.spines.right.set_visible(False)
+    ax.spines.top.set_visible(False)
+    ax.spines.left.set_visible(False)
+    ax.spines.bottom.set_visible(False)
+
+    ax.set(xticks = [], yticks = [])
+    
+    
     if save:
         print("saving...", filename)
         fig.savefig(path_to_save + filename, 
-                    dpi = 100, 
+                    dpi = 100,pad_inches = 0, 
                     bbox_inches="tight", 
                     transparent=True)
     else:
         plt.show()
         
         
-
-infile = "database/2014/024/"
-_, _, files = next(os.walk(infile))
-filename = files[100]
-
-
-    
-    
-image_visualization(infile, filename, 
-                        path_to_save = "",
-                        save = False)
