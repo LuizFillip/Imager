54f76451d17b458e879a0816127cd28586478178
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2022-12-15 23:46:04 -0300

    Updating functions and labeling positions

--- labeling.py
+++ labeling.py
@@ -1,96 +1,88 @@
-import cv2
 import os
 import matplotlib.pyplot as plt
-from image_settings import processing, get_files
+from base import getlevel, bytscl
 from image_utils import imager_fname
-import numpy as np
-import datetime
-import os
+from skimage import io
 
 def draw_labels(ax, 
-                filename, 
+                infile, 
                 fontsize = 20, 
                 color = "black"):
     
+    filename = os.path.split(infile)[-1]
+    
     d = imager_fname(filename)
     
-    ax.text(12, 500, d.time, 
+    ax.text(0, 510, d.str_time, 
             transform = ax.transData, 
             color = color, fontsize = fontsize)
     
-    ax.text(380, 500, d.date, 
+    ax.text(390, 510, d.str_date, 
             transform = ax.transData, 
             color = color, fontsize = fontsize)
     
-    ax.text(12, 30, d.site, 
+    ax.text(0, 15, d.site, 
             transform = ax.transData, 
             color = color, fontsize = fontsize)
     
-    ax.text(470, 30, d.emission, 
+    ax.text(480, 15, d.emission, 
             transform = ax.transData, 
             color = color, fontsize = fontsize)
     
-    ax.text(255, 30, "N", 
+    ax.text(256, 15, "N", 
             transform = ax.transData, 
             color = color, fontsize = fontsize + 10)
-    ax.text(480, 255, "E", 
+    ax.text(490, 256, "E", 
             transform = ax.transData, 
             color = color, fontsize = fontsize + 10)
     
    
-def remove_ticks(ax):
+def save(fig, 
+         filename, 
+         path_to_save = ""):
     
-    ax.spines.right.set_visible(False)
-    ax.spines.top.set_visible(False)
-    ax.spines.left.set_visible(False)
-    ax.spines.bottom.set_visible(False)
+    print("saving...", filename)
+    
+    plt.ioff()
+    
+    fig.savefig(path_to_save + filename, 
+                pad_inches = 0, 
+                bbox_inches = "tight", 
+                transparent = True)
+    plt.clf()   
+    plt.close()
+    return 
 
-    ax.set(xticks = [], yticks = [])
-    
-    
-    
-def image_visualization(infile, filename, 
-                        path_to_save = "",
-                        save = False, 
-                        width = 9, 
-                        height = 9):
+def visualization(infile, 
+                  width = 9, 
+                  height = 9):
     
     """Read, processing and save it"""
     
-    img = processing(infile + filename).all_processing
+    img = io.imread(infile)
     
-    if save:
-        plt.ioff()
-    
+    slevels = getlevel(img, [0.2, 0.95])
+
+    new_img = bytscl(img, 
+                      slevels[1], 
+                      slevels[0])
+            
     fig, ax = plt.subplots(figsize = (width, height))
     
-    ax.imshow(img)
+    ax.imshow(new_img, cmap = "gray")
+    
+    ax.set_axis_off()
     
     draw_labels(ax, 
-                filename, 
+                infile, 
                 fontsize = 20, 
                 color = "white")
-    
-    remove_ticks(ax)
 
-    if save:
-        print("saving...", filename)
-        fig.savefig(path_to_save + filename, 
-                    dpi = 100, 
-                    pad_inches = 0, 
-                    bbox_inches = "tight", 
-                    transparent = True)
-    else:
-        plt.show()
+    plt.show()
+    
+    return fig
         
-def main():        
-    infile = "C:\\observation\\172\\imager\\"
-    
-    
-    files = get_files(infile)
- 
-    path_to_save = "database/2014/172/"
-    for filename in files:
-        image_visualization(infile, filename, 
-                            path_to_save = path_to_save,
-                            save = True)
+infile =  "database/examples/O6_CA_20181112_000244.tif" 
+        
+visualization(infile)
+        
