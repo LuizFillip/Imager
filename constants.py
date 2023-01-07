78fd37e6cd54027861ba0dd7ded0f46ce7e71f76
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-01-07 16:32:18 -0300

    Adding sites locations names and yours coordinates into static method

--- constants.py
+++ constants.py
@@ -1,10 +1,25 @@
-
+import numpy as np
 
 class constants(object):
     
+    def __init__(self):
     # radius_equator, radius_polo (in km)
-    equator_radius = 6378.14
-    polo_radius = 6356.755
+        self.equator_radius = 6378.14
+        self.polo_radius = 6356.755
+    
+
+    def earth_radius(self, lat_obs):
+        """
+        Compute earth radius from a latitude
+        """
+        eq = self.equator_radius
+        ep = self.polo_radius
+        
+        num = pow(eq * ep, 2)
+        den = (pow(eq * np.sin(lat_obs), 2) + 
+               pow(ep * np.cos(lat_obs), 2.0))
+        
+        return np.sqrt(num / den)
     
     @staticmethod
     def projection_area(proj: int = 2) -> int:
@@ -24,7 +39,7 @@ class constants(object):
         
         """
         Altitude of emission layer (in km) 
-        OH = IR-87
+        OH (Hydroxilia) = IR-87
         """
         altitudes = {"OH": 87, 
                      "O2": 89, 
@@ -35,13 +50,31 @@ class constants(object):
         return altitudes[band]
     
     @staticmethod
-    def sites():
-        return 
+    def sites(site_name:str = "CA") -> tuple:
+        """
+        Get EMBRACE stations coordinates (longitude, latitude)
+        and your full name
+        """
+        coords = {"BJL": (-43.546917, -13.256378), 
+                  "BV": (-60.710941, 2.870188),
+                  "CA": (-36.528307, -7.381122), 
+                  "CP": (-45.009307, -22.703813), 
+                  "SMS": (-53.821934, -29.442348), 
+                  "CF": (-58.404019, -62.090483)}
+        
+        names = {"BJL": "Bom Jesus da Lapa", 
+                  "BV": "Boa Vista",
+                  "CA": "Sao Joao do Cariri", 
+                  "CP": "Cachoeira Paulista", 
+                  "SMS": "Sao Martinho da Serra", 
+                  "CF": "Comandante Ferraz"}
+        
+        return names[site_name], coords[site_name]
     
     
 def main():
     c = constants()
     
-    print(c.emission_band())
-    print(c.projection_area())
+    #print(c.emission_band())
+    print(c.sites())
     
