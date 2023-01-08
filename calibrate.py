a2c993e5844e9f3a800bc4fcfd510e6b2d43aa20
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-01-08 14:56:08 -0300

    join elements in class

--- calibrate.py
+++ calibrate.py
@@ -2,7 +2,7 @@ import datetime
 import os
 import json 
 import pandas as pd
-
+from image_utils import date_from_doy, remove_values
 import sys
 os.path.dirname(sys.executable)
 from pathlib import Path
@@ -16,73 +16,104 @@ def get_date_from_folder(folder: str) -> datetime.date:
     
     return datetime.date(year, 1, 1) + datetime.timedelta(doy - 1)
 
-def convert_infos_into_dict(absolute_path: str, folder: str) -> dict:
-    """Read .dat files and update into dictionary"""
-    filename = [fi for fi in next(os.walk(absolute_path + folder))[-1] 
-            if fi.endswith(".dat")][0]
-    
-    with open(f"{absolute_path}{folder}/{filename}", 
-              encoding='latin-1') as f:
-        
-        dat = [line.strip() for line in f.readlines()]
-    
-    out_dict = {}
-    for elem in dat:
-        if "--" in elem:
-            break
-        elif elem == "":
-            pass
-        else:
-            args = elem.split(": ")
-            out_dict.update({args[0] : args[1].strip()})
-            
-    return out_dict
-        
 
-def calibration_infos_by_date(infile: str, folder: str) -> dict:
-    """Create an dictonary with date of calibration"""
-    date = get_date_from_folder(folder)
-    
-    return {str(date): convert_infos_into_dict(infile, 
-                                               folder)}
-
-def run_for_all_files(infile: str) -> dict:
-    """Get path with all times calibration and append to one single dictionary"""
-    _, folders, _ = next(os.walk(infile))
-
-    out_dict = {}
-
-    for folder in folders:
-
-        out_dict.update(calibration_infos_by_date(infile, 
-                                                  folder))
-
-    return out_dict
-
-
-
-def save_json(infile: str, 
-              name: str = "cariri.json") -> None:
-    """Save results"""
-    with open(infile + name, 'w') as f:
-        json.dump(run_for_all_files(infile), f)
-        
-        
 
 def get_calibration(time: datetime.datetime, 
-                     filename:str = "CA.json") -> dict:
+                    site:str = "CA") -> dict:
     """Open json file for the last calibration for the time"""
     
     infile = os.path.join(str(Path.cwd()), "calibrate")
-    dat = json.load(open(os.path.join(infile, filename)))
+    dat = json.load(open(os.path.join(infile, site, f"{site}.json")))
 
-    dates = pd.to_datetime(list(dat.keys()))
+    ts = pd.to_datetime(list(dat.keys()))
     
-    for num in range(len(dates) - 1):
+    for num in range(len(ts) - 1):
         
-        if (dates[num] > time) and (dates[num + 1] < time):
-            return dat[str(dates[num + 1].date())]
+        dt1 = ts[num].date()
+        dt2 = ts[num + 1].date()
+        if (dt1 > time) and (dt2 < time):
+            return dat[str(dt2)]
         
-        elif (time > max(dates)):
-            return dat[str(max(dates).date())]
+        elif (time > max(ts)):
+            return dat[str(max(ts).date())]
         
+
+
+class load(object):
+    
+    def __init__(self, infile, folder):
+        
+        path_of_folder = os.path.join(infile, folder)
+        
+        filename = [fi for fi in next(os.walk(path_of_folder))[-1] 
+                    if fi.endswith(".dat")][0]
+        
+        
+        dat = open(os.path.join(path_of_folder, filename), 
+                   encoding = 'latin-1').read()
+
+        sec = dat.find("Nr.")
+        
+        self.cal_section = dat[sec:].split("\n")
+        self.dat_section = dat[:sec].split("\n")
+        
+        
+    @property
+    def result(self):
+        """Return site parameters and lens functions result 
+        and update into dictionary"""
+        out_dict = {}
+        for elem in self.dat_section:
+            if "--" in elem:
+                break
+            elif elem == "":
+                pass
+            else:
+                args = elem.split(": ")
+                out_dict.update({args[0] : args[1].strip()})
+        
+        return out_dict
+    
+    @property
+    def data(self):
+        """Return calibrate data from stars"""
+        header = remove_values(self.cal_section[0].split("  "))
+    
+        body = [i.split() for i in self.cal_section[1:-1]]
+        return  pd.DataFrame(body, columns = header)
+
+
+
+site = "CA"
+
+def run_for_all_files(site: str = "CA", 
+                      save: bool = True) -> dict:
+    
+    """Get path with all times calibration 
+    and append to one single dictionary"""
+    infile = os.path.join(str(Path.cwd()), 
+                          "calibrate", 
+                           site)
+    
+    _, folders, _ = next(os.walk(infile))
+    
+    
+    out_dict = {}
+    
+    for folder in folders:
+        dat = load(infile, folder)
+        date = get_date_from_folder(folder)
+        
+        out_dict.update({str(date): dat.result})
+        
+        
+    if save:
+        with open(infile + site + ".json", 'w') as f:
+            json.dump(out_dict, f)
+
+    return out_dict
+
+
+date = date_from_doy(2011, 264)
+
+
