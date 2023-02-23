b202b5cad3b1456bbe581fe34625b65a43b37495
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-02-23 15:37:55 -0300

    run all imgs

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from AllSky.base import getlevel, bytscl
 from AllSky.labeling import visualization, save_img
 
 from skimage import io
-from setup import images_to_movie
+
 
 files = p("AllSky").get_files_in_dir("2023")
 
@@ -26,5 +26,46 @@ def processing_img(fname):
     return visualization(new_img, fname, 
                             width = 10, 
                             height = 10)
+import os
+def process_all_images(files): 
+    
+    files  = [f for f in files if f.endswith("tif")]
+    
+    for fname in files:
+        
+        save_in = fname.replace("tif", "png")
+        
+        name = os.path.split(save_in)[-1]
+        print("processing...", name)
+    
+        save_img(processing_img(fname), save_in)
+        
+          
+
+#process_all_images(files)
 
 
+def images_to_gif():
+    import glob
+    import contextlib
+    from PIL import Image
+    
+    # filepaths
+    fp_in = "database/AllSky/2023/*.png"
+    fp_out = "image.gif"
+    
+    # use exit stack to automatically close opened images
+    with contextlib.ExitStack() as stack:
+    
+        # lazily load images
+        imgs = (stack.enter_context(Image.open(f))
+                for f in sorted(glob.glob(fp_in)))
+    
+        # extract  first image from iterator
+        img = next(imgs)
+    
+        img.save(fp=fp_out, format='GIF', append_images=imgs,
+                 save_all=True, duration=200, loop=0, 
+                 dpi=(300, 300), quality=150)
+
+images_to_gif()
