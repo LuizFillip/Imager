315b3fee71f6823b48fd46b2764258f102bd202a
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-10-20 17:47:49 -0300

    process along

---
+++ src/process.py
@@ -0,0 +1,52 @@
+
+
+def process_visualize(path_in, size = 10):
+    image = processing_img(
+        path_in, 
+        hori_flip = False,
+        vert_flip = True
+        )
+    
+    
+    return im.visualization(
+                remove_stars(image), 
+                path_in, 
+                width = size, 
+                height = size
+                )
+
+
+infile = 'D:\\img\\'
+import base as b 
+
+for folder in os.listdir(infile)[:-1]:
+    
+    folder_out =  path_out = os.path.join(
+         'D:\\img\\PRO\\', 
+         folder
+         )
+    
+    b.make_dir(folder_out)
+    
+    
+    path_folder = os.path.join(infile, folder)
+        
+    for fname in os.listdir(path_folder):
+        
+        if 'DARK' in fname:
+            pass
+        else:
+        
+            path_in = os.path.join(path_folder, fname)
+            
+    
+            save_in = os.path.join(
+               folder_out, 
+               fname.replace('tif', 'png')
+               )
+            
+            
+            print('processing', fname)
+            fig = process_visualize(path_in, size = 10)
+            
+            save_img(fig, save_in)
