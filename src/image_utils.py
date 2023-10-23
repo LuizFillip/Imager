d1bb1f7ad04e840e91bc13627fe086e9c5fbbac7
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-10-23 14:30:46 -0300

    moving functions to utilities

--- src/image_utils.py
+++ src/image_utils.py
@@ -5,21 +5,52 @@ import pandas as pd
 
 
 
+def fn2datetime(filename):
+
+    infos = filename[:-4].split("_") 
+    date_time = infos[2] + " " + infos[-1]
+    form = '%Y%m%d %H%M%S'
+    return dt.datetime.strptime(date_time, form)
+      
+
+class imager_fname(object):
     
+    """Get datetime from filename (EMBRACE format)"""
     
+    def __init__(self, filename):
+        
+        filename = os.path.split(filename)[-1]
+  
+        infos = filename[:-4].split("_")
+        date = infos[2]
+        time = infos[-1]
+        self.emission = infos[0]
+        self.site = infos[1]
+        self.datetime = dt.datetime.strptime(
+            date + " " + time, 
+            '%Y%m%d %H%M%S')
+  
+    @property
+    def str_time(self):
+        return self.datetime.strftime("%H:%M:%S UT")
     
+    @property
+    def str_date(self):
+        return self.datetime.strftime("%d/%m/%Y")
+
 def get_files(infile: T.TextIO, extension = ".tif"): 
     """
     get all files in directory like glob
     """
-    _, _, files = next(os.walk(infile))
     
-    return [f for f in files if f.endswith(extension)]
+    return [f for f in os.listdir(infile) if 
+            f.endswith(extension)]
 
 def date_from_doy(year: int, doy:int) -> dt.datetime:
     """Return date from year and doy"""
-    return pd.Timestamp(dt.date(year, 1, 1) +
-                        dt.timedelta(doy - 1))
+    return pd.Timestamp(
+        dt.date(year, 1, 1) +
+        dt.timedelta(doy - 1))
 
 
 
@@ -45,14 +76,4 @@ def convert_tif_to_png(filename, path = None):
         os.rename(src, dest)   
     except RuntimeError:
         print("All files was converted")
-        
-path_in = "database/AllSky/2023/"
-
-def run_in_path(path_in):
-
-    _, _, files = next(os.walk(path_in))
-    
-    for filename in files:
-        
-        convert_tif_to_png(filename, path = path_in)
-        
+ 
